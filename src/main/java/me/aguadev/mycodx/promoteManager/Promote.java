ac3157f3e57b4cf89e1b1aeded2a24166403bcc5
7Str1kes <imdemoted721xd@gmail.com>
2025-03-09 20:56:07 +0100

    Update: - Removed unnecessary imports in the main class

--- src/main/java/me/aguadev/mycodx/promoteManager/Promote.java
+++ src/main/java/me/aguadev/mycodx/promoteManager/Promote.java
@@ -1,16 +1,11 @@
 package me.aguadev.mycodx.promoteManager;
 
 import lombok.Getter;
-import me.aguadev.mycodx.promoteManager.modules.command.MainCommand;
 import me.aguadev.mycodx.promoteManager.modules.provider.Files;
 import me.aguadev.mycodx.promoteManager.modules.rank.RankManager;
-import me.aguadev.mycodx.promoteManager.modules.rank.command.DowngradeCommand;
-import me.aguadev.mycodx.promoteManager.modules.rank.command.PromoteCommand;
 import me.aguadev.mycodx.promoteManager.modules.provider.Manager;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
@@ -37,7 +32,5 @@ public final class Promote extends JavaPlugin {
     }
 
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+    public void onDisable() {}
 }
